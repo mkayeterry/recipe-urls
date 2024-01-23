634d763eeef51af2cc0ac63ff1f0bc8a070f1c4c
Melissa Terry <mkterry@ucsb.edu>
2024-01-23 14:34:53 -0800

    Altered main.py such that it takes in a single base url rather than having ann option for a list

--- recipe_urls/main.py
+++ recipe_urls/main.py
@@ -21,50 +21,45 @@ SCRAPER_CLASSES = {
     # 'nytimes': NyTimesScraper
 }
 
-def get_recipe_urls(base_urls):
+def get_recipe_urls(base_url):
     """
     Extracts recipe information from a list of base URLs using specific scraper classes.
 
     Args:
-    - base_urls (str or list): A single base URL or a list of base URLs to extract recipes from.
+    - base_url (str): A single base URL to extract recipes from.
 
     Returns:
     - list: A list of recipe URLs
     """
-    # Ensure base_urls is a list for uniform processing
-    if not isinstance(base_urls, list):
-        base_urls = [base_urls]
 
-    # Initialize an empty list to store recipe URLs
-    compiled_recipe_urls = []
-
-    # Iterate through each base URL
-    for base_url in base_urls:
-        # Categorize the URL to determine the appropriate scraper class
-        site_origin = categorize_url(base_url)
+    # Categorize the URL to determine the appropriate scraper class
+    site_origin = categorize_url(base_url)
+    
+    try:
+        # Get the scraper class from the dictionary
+        scraper_class = SCRAPER_CLASSES[site_origin]
+        print(f"[main.py] {base_url} successfully matched to {scraper_class}.")
         
-        try:
-            # Get the scraper class from the dictionary
-            scraper_class = SCRAPER_CLASSES[site_origin]
-            print(f"[main.py] {base_url} successfully matched to {scraper_class}.")
-            
-            # Create an instance and call the scrape method
-            scraper_instance = scraper_class(base_url)
-            
-            # Append the recipe URLs to the list
-            recipe_urls = scraper_instance.scrape()
-            print(f"[main.py] {len(recipe_urls)} recipe URLs scraped from {base_url}.")
-            compiled_recipe_urls.extend(recipe_urls)
+        # Create an instance and call the scrape method
+        scraper_instance = scraper_class(base_url)
+        
+        # Append the recipe URLs to the list
+        recipe_urls = scraper_instance.scrape()
+        print(f"[main.py] {len(recipe_urls)} recipe URLs scraped from {base_url}.")
 
-        except KeyError:
-            print(f"[main.py] Error: Scraper class not found for URL '{base_url}'.")
+    except KeyError:
+        print(f"[main.py] Error: Scraper class not found for URL '{base_url}'.")
 
-    return compiled_recipe_urls
+    return recipe_urls
 
 
 
 # Testing 
-# url = ['https://www.baking-sense.com', 'https://www.allrecipes.com', 'http://www.afghankitchenrecipes.com']
-# urls = get_recipe_urls(url)
+urls = ['https://www.baking-sense.com', 'https://www.allrecipes.com', 'http://www.afghankitchenrecipes.com']
+compiled_urls = []
+
+for url in urls:
+    recipes = get_recipe_urls(url)
+    compiled_urls.append(recipes)
 
 
