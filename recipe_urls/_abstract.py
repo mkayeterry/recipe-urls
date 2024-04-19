a80f8c8f50b8ae0fb620cec92802fbcc12e81cac
Joey <joey.kennedy1@gmail.com>
2024-04-19 13:52:35 -0400

    Switched from `httpx` to `requests`

--- recipe_urls/_abstract.py
+++ recipe_urls/_abstract.py
@@ -1,39 +1,37 @@
 from typing import List, Optional
-import httpx
+import requests
 from bs4 import BeautifulSoup
-
 
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:86.0) Gecko/20100101 Firefox/86.0"
 }
 
 class AbstractScraper:
-    def __init__(self, base_url: str | None = None, html: str | None = None):
+    def __init__(self, base_url: Optional[str] = None, html: Optional[str] = None):
         self.base_url = base_url
 
         if not html:
             try:
-                response = httpx.get(url=self.base_url, headers=HEADERS)
+                response = requests.get(url=self.base_url, headers=HEADERS)
                 response.raise_for_status()
 
                 self.html = response.content
                 self.soup = BeautifulSoup(self.html, "html.parser")
 
-            except (httpx.HTTPError, httpx.ReadTimeout) as e:
-                if isinstance(e, httpx.HTTPError) and e.response.status_code == 403:
-                    raise httpx.HTTPError(f"Access to {self.base_url} is forbidden (403).") from e
-                elif isinstance(e, httpx.ReadTimeout):
-                    raise TimeoutError(f"Request timed out. {self.base_url}") from e
+            except requests.HTTPError as e:
+                if e.response.status_code == 403:
+                    raise Exception(f"Access to {self.base_url} is forbidden (403).") from e
                 else:
-                    raise
+                    raise Exception(f"HTTP error occurred: {e.response.status_code}.") from e
+            except requests.RequestException as e:
+                raise Exception(f"Request failed: {e}.") from e
 
             except Exception as e:
-                raise RuntimeError(f"Unexpected error accessing {self.base_url}. {e}") from e
+                raise Exception(f"Unexpected error accessing {self.base_url}: {e}.") from e
 
         else:
             self.html = html
             self.soup = BeautifulSoup(self.html, "html.parser")
-
 
     @classmethod
     def host(cls) -> Optional[str]:
